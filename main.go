7440b8de47bf2deb2878d42a084c9b7c4fe1110b
Marcos Ribeiro <mrj84@outlook.com>
2021-05-04 14:19:44 -0300

    map data type

--- main.go
+++ main.go
@@ -3,24 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	slice := []int{1, 2, 3}
+	m := map[string]int{"foo": 42}
+	fmt.Println(m)
+	fmt.Println(m["foo"])
 
-	fmt.Println(slice)
+	m["foo"] = 33
+	fmt.Println(m)
 
-	slice = append(slice, 4, 27, 12)
-
-	fmt.Println(slice)
-
-	s2 := slice[1:] // slice the variable starting at item index 1 and proceeding to the end.
-	s3 := slice[:2] // set an ending index (not incluiding the index 2, just 0 and 1)
-	s4 := slice[1:2]
-
-	fmt.Println(s2, s3, s4)
+	delete(m, "foo")
+	fmt.Println(m)
 }
 
 /*
 $ go run main.go
-[1 2 3]
-[1 2 3 4 27 12]
-[2 3 4 27 12] [1 2] [2]
+
+map[foo:42]
+42
+map[foo:33]
+map[]
 */
