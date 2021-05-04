3f00a79eac0e1242539a362206ac7034e38bb7fb
Marcos Ribeiro <mrj84@outlook.com>
2021-05-04 12:20:06 -0300

    slice pt 1

--- main.go
+++ main.go
@@ -3,19 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var arr [3]int
-	arr[0] = 1
-	arr[1] = 2
-	arr[2] = 3
-	fmt.Println(arr)
+	arr := [3]int{1, 2, 3}
 
-	arr2 := [3]int{1, 2, 3}
-	fmt.Println(arr2)
-	// fmt.Println(arr[4]) // ./main.go:16:17: invalid array index 4 (out of bounds for 3-element array)
+	slice := arr[:]
+
+	arr[1] = 42
+	slice[2] = 27
+
+	fmt.Println(arr, slice)
 }
 
 /*
 $ go run main.go
-[1 2 3]
-[1 2 3]
+[1 42 27]
+[1 42 27]
 */
