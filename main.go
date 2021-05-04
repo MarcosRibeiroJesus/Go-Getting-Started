f8f3b855194a284c6f3251ea983a75df01a389e3
Marcos Ribeiro <mrj84@outlook.com>
2021-05-04 20:02:41 -0300

    Panic Function

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 func main() {
-	// wellKnownPorts := map[string]int{"http": 80, "https": 443}
-	slice := []int{1, 2, 3}
-	for i, v := range slice {
-		println(i, v)
-	}
+	println("Starting web server...")
+
+	// do important things
+	panic("Something bad just happened...")
+
+	println("Web server started") // unreachable code
 }
 
 /*
-$ go run main.go with map
+go run main.go // panic
 
-https 443
-http 80
+Starting web server...
+panic: Something bad just happened...
 
-$ go run main.go with slice
+goroutine 1 [running]:
+main.main()
+        /home/marcosribeiro/Documents/Learning/Go/Go-Getting-Started/main.go:7 +0x5c
+exit status 2
 
-0 1
-1 2
-2 3
+go run main.go // without panic
+
+Starting web server...
+Web server started
 */
