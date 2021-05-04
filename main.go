7509ce46e21f6267397ed08d7054ee5c604733c0
Marcos Ribeiro <mrj84@outlook.com>
2021-05-04 12:15:22 -0300

    arrays

--- main.go
+++ main.go
@@ -2,23 +2,20 @@ package main
 
 import "fmt"
 
-const (
-	first  = iota + 6  // first = 6 && iota = 0 prints 6
-	second = 2 << iota // second = 2 << 2 || 2 * iota(1) * 2 = 4 && iota = 1 + 6(first) prints 4
-	third  = 3 << iota // third 3 << 3 ??? prints 12
-	fourth             // ?????? prints 24
-)
-
-const (
-	fifth = iota
-	sixth
-)
-
 func main() {
-	fmt.Println(first, second, third, fourth, fifth, sixth)
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 2
+	arr[2] = 3
+	fmt.Println(arr)
+
+	arr2 := [3]int{1, 2, 3}
+	fmt.Println(arr2)
+	// fmt.Println(arr[4]) // ./main.go:16:17: invalid array index 4 (out of bounds for 3-element array)
 }
 
 /*
 $ go run main.go
-6 4 12 24 0 1
+[1 2 3]
+[1 2 3]
 */
