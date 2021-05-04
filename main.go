e87442ee0ea1c9a5b0742562718fa302bb91fbf1
Marcos Ribeiro <mrj84@outlook.com>
2021-05-04 14:13:20 -0300

    slices pt 2

--- main.go
+++ main.go
@@ -3,18 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	arr := [3]int{1, 2, 3}
+	slice := []int{1, 2, 3}
 
-	slice := arr[:]
+	fmt.Println(slice)
 
-	arr[1] = 42
-	slice[2] = 27
+	slice = append(slice, 4, 27, 12)
 
-	fmt.Println(arr, slice)
+	fmt.Println(slice)
+
+	s2 := slice[1:] // slice the variable starting at item index 1 and proceeding to the end.
+	s3 := slice[:2] // set an ending index (not incluiding the index 2, just 0 and 1)
+	s4 := slice[1:2]
+
+	fmt.Println(s2, s3, s4)
 }
 
 /*
 $ go run main.go
-[1 42 27]
-[1 42 27]
+[1 2 3]
+[1 2 3 4 27 12]
+[2 3 4 27 12] [1 2] [2]
 */
